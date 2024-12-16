e1ec323148390e7f0e6f6ef510d8ae0bb92283b9
AZubochenko <azubochenko@ibs.ru>
2024-12-16 20:37:49 +0300

    class work commit

---
+++ lesson_1/first_part.py
@@ -0,0 +1,23 @@
+#print("hi")
+# print(5 + 7)
+# print(5 - 7)
+# print(5 * 7)
+# print(7 / 7)
+# print(49 ** 0.5)
+# print(8 // 6)
+# print(8 % 6)
+
+# x = "50"
+# y = '5'
+# z = x + y
+# z *= 2
+# z = z + z
+#+=, -=, /=, *=
+# x = float(x)
+# z = x/y
+# z = int(z)
+# # z = str(z)
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(x, y, z)
