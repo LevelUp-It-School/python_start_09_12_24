87e26d8ce257ea562f4e3b2a61e719e2425ef298
AZubochenko <azubochenko@ibs.ru>
2025-02-06 21:40:54 +0300

    message

--- lesson_14/first_part.py
+++ lesson_14/first_part.py
@@ -1,4 +1,4 @@
-assert 2 + 2 == 5
+# assert 2 + 2 == 5
 
 #class Transformer
 # {"I":1, "V": 5, "X":10, "L": 50,"C":100, "D": 500, "M":1000}
@@ -7,12 +7,22 @@ assert 2 + 2 == 5
 IV - 4
 VI - 6
 '''
+class Transformer:
+    def __init__(self, roman_number = "XIV"):
+        self.roman_number = roman_number
 
-XVI
-16
-XV
-VI
+def roman_to_decimal(roman_number):
+    summ = 0
+    tallies = {"I":1, "V": 5, "X":10, "L": 50,"C":100, "D": 500, "M":1000}
+    for i in range(len(roman_number) - 1):
+        left = roman_number[i]
+        right = roman_number[i+1]
+        if tallies[left] < tallies[right]:
+            summ -= tallies[left]
+        else:
+            summ += tallies[left]
+    summ += tallies[roman_number[-1]]
+    return summ
 
-
-XIV
-14
+tal = Transformer("MMMXIV")               
+print(roman_to_decimal(tal.roman_number))
